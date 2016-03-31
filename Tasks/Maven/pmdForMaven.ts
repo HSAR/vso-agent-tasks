591922c2a5af4d1abde1d9982cc253a40ade7ceb
Shu Sam Chen <sschen@microsoft.com>
2016-03-31 16:32:44 +0100

    Code review comments for adding a build summary to PMD code analysis on Maven.

--- Tasks/Maven/pmdForMaven.ts
+++ Tasks/Maven/pmdForMaven.ts
@@ -17,38 +17,67 @@ export function applyPmdArgs(mvnRun: trm.ToolRunner):void {
 
 // Extract analysis results from PMD output file.
 // Takes the current working directory and returns an AnalysisResult data class.
+// Task fails if the HTML or XML outputs were not found.
 // @param sourcesDirectory - The absolute location of the root source directory.
-// @param overrideFolder - (Optional) If true, look only in the location specified by the first argument.
 export function processPmdOutput(sourcesDirectory:string) : ar.AnalysisResult {
     var result:ar.AnalysisResult = new ar.AnalysisResult();
     result.toolName = 'PMD';
 
-    // Verify the existence of pmd.html file - it is also written to a well-known location
-    var pmdHtmlFilePath = path.join(sourcesDirectory, '/target/site/pmd.html');
-    tl.checkPath(pmdHtmlFilePath, "./target/site/pmd.html");
-    result.htmlFilePath = pmdHtmlFilePath;
-
-    // Request pmd.xml file for reading - it is written to a well-known location
     var pmdXmlFilePath = path.join(sourcesDirectory, '/target/pmd.xml');
-    tl.checkPath(pmdXmlFilePath, "./target/pmd.xml");
-    var pmdXmlFileContents = fs.readFileSync(pmdXmlFilePath, 'utf-8');
+    result = processPmdXml(result, pmdXmlFilePath);
+
+    // if there are no violations, there will be no HTML report
+    if (result.totalViolations > 0) {
+        var pmdHtmlFilePath = path.join(sourcesDirectory, '/target/site/pmd.html');
+        result = processPmdHtml(result, pmdHtmlFilePath);
+    }
+
+    return result;
+}
+
+// Verifies the existence of the HTML output file.
+// Modifies the relevant field within the returned object accordingly.
+// Task fails if the file cannot be found.
+function processPmdHtml(analysisResult:ar.AnalysisResult, path:string):ar.AnalysisResult {
+    // Task fails if the file cannot be found.
+    if (!tl.exist(path)) {
+        tl.error("Could not find PMD HTML output at expected location: " + path);
+        tl.error("Check that PMD ran successfully and that it is writing to the default location.");
+        tl.checkPath(path, path);
+    }
+    analysisResult.htmlFilePath = path;
+    return analysisResult;
+}
+
+// Verifies the existence of the XML output file and parses its contents.
+// Modifies the relevant fields within the returned object accordingly.
+// Task fails if the file cannot be found.
+function processPmdXml(analysisResult:ar.AnalysisResult, path:string):ar.AnalysisResult {
+    if (!tl.exist(path)) {
+        tl.error("Could not find PMD XML output at expected location: " + path);
+        tl.error("Check that PMD ran successfully and that it is writing to the default location.");
+        tl.checkPath(path, path);
+    }
+
+    var pmdXmlFileContents = fs.readFileSync(path, 'utf-8');
     xml2js.parseString(pmdXmlFileContents, function (err, data) {
         if (data) {
-            result.xmlFilePath = pmdXmlFilePath;
+            analysisResult.xmlFilePath = path;
+
+            analysisResult.filesWithViolations = data.pmd.file.length;
+            analysisResult.totalViolations = 0;
+
+            if (analysisResult.filesWithViolations < 1) {
+                // Exit quickly if no violations found
+                return analysisResult;
+            }
+
+            data.pmd.file.forEach(function (file) {
+                analysisResult.totalViolations += file.violation.length;
+            });
+        } else {
+            tl.error("Failed to parse XML output from PMD.")
         }
-
-        result.filesWithViolations = data.pmd.file.length;
-        result.totalViolations = 0;
-
-        if (result.filesWithViolations < 1) {
-            // Exit quickly if no violations found
-            return result
-        }
-
-        data.pmd.file.forEach(function (file) {
-            result.totalViolations += file.violation.length;
-        });
-
     });
-    return result;
+    return analysisResult;
 }
